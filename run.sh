c788d5b5db3a4655a2adc6c3dbb2e81c7e3e12ab
Soham <sohambadheka008@gmail.com>
2017-04-07 18:52:34 -0700

    Modified run.sh with python file execution

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@
 # the contents of this script could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
-python ./src/process_log.py ./log_input/log.txt ./log_output/hosts.txt ./log_output/hours.txt ./log_output/resources.txt ./log_output/blocked.txt
+python ./src/hosts.py ./log_input/log.txt ./log_output/hosts.txt
+python ./src/hours.py ./log_input/log.txt ./log_output/hours.txt
+python ./src/resources.py ./log_input/log.txt ./log_output/resources.txt
 
+# 4th feature is not optimized so, I am not uploading that in the code section
+# python ./src/blocked.py ./log_input/log.txt ./log_output/blocked.txt
